afdf230738682d07a163e11bad7d61e072a730ca
Brent <brentbaughan1@gmail.com>
2021-07-03 20:01:29 -0400

    continued work on <Navbar>; copied over propic and previous navbar css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import "./style.css"
 
 function Navbar() {
-    
+    return (
+    <header>
+            <h1>Brent B.</h1>
+            <img src="%PUBLIC_URL%/recent-profile.jpg" alt="Brent"/>
+            <nav>
+                <ul>
+                    <li><a href="#AboutMe">About Me</a></li>
+                    <li><a href="#AboutWork">About My Work</a></li>
+                    <li><a href="#AboutMe">Back to Top</a></li> 
+                </ul>
+            </nav>
+    </header>
+    )
 }
+
+export default Navbar;
